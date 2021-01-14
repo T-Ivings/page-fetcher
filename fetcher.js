45073d798eebae876357b7cc1fe460ec1e067204
Taylor <ivingstaylor@gmail.com>
2021-01-14 04:05:11 +0000

    added better comments

--- fetcher.js
+++ fetcher.js
@@ -11,10 +11,10 @@ const webGet = function (website, directory) {
     console.log('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     console.log('body:', body); // Print the HTML for the specified homepage.
-    fs.writeFile(directory, body, 'utf-8', (err) => {
-      if (err) throw err;
-      bytes = fs.statSync(directory);
-      console.log(`Downloaded and saved ${bytes.size} bytes to ${directory}`)
+    fs.writeFile(directory, body, 'utf-8', (err) => { //writes to directory, writes the body, in readable code, also can error
+      if (err) throw err; //if its an error throw an error
+      bytes = fs.statSync(directory); //how many bites is directory
+      console.log(`Downloaded and saved ${bytes.size} bytes to ${directory}`) //print that info out for the boys and gals
     })
   })
 };
