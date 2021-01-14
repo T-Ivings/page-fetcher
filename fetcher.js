68721c3a0bdb43ddcdad4b0d0c4d2f1eae51d6d6
Taylor <ivingstaylor@gmail.com>
2021-01-14 17:17:44 +0000

    further refactoring

--- fetcher.js
+++ fetcher.js
@@ -7,7 +7,7 @@ return process.argv.slice(2, 4)
 }
 
 const webGet = function (website, directory) {
-  request(website, (error, response, body) => {
+  request(website, (error, body) => {
     if (error) {console.log('error:', error)}; // Print the error if one occurred
     fs.writeFile(directory, body, 'utf-8', (err) => { //writes to directory, writes the body, in readable code, also can error
       if (err) throw err; //if its an error throw an error
